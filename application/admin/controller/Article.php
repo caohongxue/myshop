b5959ae3ca5460555a7ec6ac1f0fdb07c11d63ec
caohongxue <416592729@qq.com>
2018-04-19 11:56:59 +0800

    商品属性完成

--- application/admin/controller/Article.php
+++ application/admin/controller/Article.php
@@ -4,7 +4,7 @@ namespace app\admin\controller;
 
 use think\Controller;
 use think\Request;
-
+use app\admin\model\Article as ArticleModel;
 class Article extends Controller
 {
     /**
@@ -14,7 +14,16 @@ class Article extends Controller
      */
     public function index()
     {
-        //
+        $model=new ArticleModel();
+        $list=$model->paginate('10');
+        foreach ($list as $value){
+            if($value['is_show']==1){
+                $value['is_show_text']='展示';
+            }else{
+                $value['is_show_text']='不展示';
+            }
+        }
+        return $this->fetch('index',['list'=>$list]);
     }
 
     /**
@@ -22,9 +31,18 @@ class Article extends Controller
      *
      * @return \think\Response
      */
-    public function create()
+    public function desc()
     {
-        //
+        $model=new ArticleModel();
+        $list=$model->order('name')->paginate('10');
+        foreach ($list as $value){
+            if($value['is_show']==1){
+                $value['is_show_text']='展示';
+            }else{
+                $value['is_show_text']='不展示';
+            }
+        }
+        return $this->fetch('index',['list'=>$list,'desc'=>1]);
     }
 
     /**
@@ -33,9 +51,10 @@ class Article extends Controller
      * @param  \think\Request  $request
      * @return \think\Response
      */
-    public function save(Request $request)
-    {
-        //
+    public function table($id){
+        $table=ArticleModel::get($id)->value('table_name');
+        $c=ucfirst($table);
+        $this->redirect($c.'/index');
     }
 
     /**
