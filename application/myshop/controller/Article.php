cd39db0ce899336a0cfa548b13a91cbfb668ae1e
dizhengyang <yangyang@qq.com>
2018-04-20 16:06:04 +0800

    邸正扬第二次提交版本

--- application/myshop/controller/Article.php
+++ application/myshop/controller/Article.php
@@ -5,6 +5,7 @@ namespace app\myshop\controller;
 use think\Controller;
 use think\Db;
 use think\Request;
+use think\Session;
 
 class Article extends Controller
 {
@@ -20,14 +21,13 @@ class Article extends Controller
             ->limit(8)
             ->select();
         $brand=Db::table('brand')
-            ->where(['is_show'=>1])
-            ->limit(6)
+            ->where(['b_id'=>2,'is_show'=>1])
             ->select();
-        return $this->fetch('index',['iphone'=>$iphone,'brand'=>$brand]);
+        return $this->fetch('index',['iphone'=>$iphone,'brand_name'=>$brand]);
     }
 
     /**
-     * 显示创建资源表单页.
+     * 显示商品详情
      *
      * @return \think\Response
      */
@@ -43,55 +43,146 @@ class Article extends Controller
 
 
 
-
-    public function create()
+/*
+ * $id:品牌id
+ * */
+    public function saler($id)
     {
-        //
+        $mobile=Db::table('brand')->where(['id'=>$id,'is_show'=>1])->find();
+        if($mobile['b_id']==1){
+            $mobile['b_id']='电脑';
+        }elseif ($mobile['b_id']==2){
+            $mobile['b_id']='手机';
+        }elseif ($mobile['b_id']==3) {
+            $mobile['b_id'] = '电脑配件';
+        }elseif ($mobile['b_id']==4) {
+            $mobile['b_id'] = '手机配件';
+        }
+        $ips=Db::table('goods')->where(['brand_id'=>$id])->paginate(8);
+        $parts=Db::table('goods')
+            ->alias('a')
+            ->join('brand w','a.cat_id = w.b_id')
+            ->where(['w.b_id'=>4])
+            ->paginate(2);
+//        var_dump($parts);die;
+        return $this->fetch('saler',['mobile'=>$mobile,'ips'=>$ips,'parts'=>$parts]);
     }
 
     /**
-     * 保存新建的资源
-     *
-     * @param  \think\Request  $request
-     * @return \think\Response
+     * 综合排序
      */
-    public function save(Request $request)
+    public function click($id)
     {
-        //
+        $mobile=Db::table('brand')->where(['id'=>$id,'is_show'=>1])->find();
+        if($mobile['b_id']==1){
+            $mobile['b_id']='电脑';
+        }elseif ($mobile['b_id']==2){
+            $mobile['b_id']='手机';
+        }elseif ($mobile['b_id']==3) {
+            $mobile['b_id'] = '配件';
+        }
+
+        $ips=Db::table('goods')
+            ->where(['brand_id'=>$id])
+            ->order('click_name','desc')
+            ->paginate(8);
+
+        $parts=Db::table('goods')
+            ->alias('a')
+            ->join('brand w','a.cat_id = w.b_id')
+            ->where(['w.b_id'=>4])
+            ->paginate(2);
+
+        return $this->fetch('saler',['mobile'=>$mobile,'ips'=>$ips,'parts'=>$parts]);
     }
 
     /**
-     * 显示指定的资源
-     *
-     * @param  int  $id
-     * @return \think\Response
+     * 按销量排序
      */
-    public function read($id)
+    public function sale($id)
     {
-        //
+        $mobile=Db::table('brand')->where(['id'=>$id,'is_show'=>1])->find();
+        if($mobile['b_id']==1){
+            $mobile['b_id']='电脑';
+        }elseif ($mobile['b_id']==2){
+            $mobile['b_id']='手机';
+        }elseif ($mobile['b_id']==3) {
+            $mobile['b_id'] = '配件';
+        }
+
+        $ips=Db::table('goods')
+            ->where(['brand_id'=>$id])
+            ->order('sale_number','desc')
+            ->paginate(8);
+
+        $parts=Db::table('goods')
+            ->alias('a')
+            ->join('brand w','a.cat_id = w.b_id')
+            ->where(['w.b_id'=>4])
+            ->paginate(2);
+
+        return $this->fetch('saler',['mobile'=>$mobile,'ips'=>$ips,'parts'=>$parts]);
     }
 
     /**
-     * 显示编辑资源表单页.
-     *
-     * @param  int  $id
-     * @return \think\Response
+     * 按价格排序
      */
-    public function edit($id)
+    public function price($id)
     {
-        //
+        $mobile=Db::table('brand')->where(['id'=>$id,'is_show'=>1])->find();
+        if($mobile['b_id']==1){
+            $mobile['b_id']='电脑';
+        }elseif ($mobile['b_id']==2){
+            $mobile['b_id']='手机';
+        }elseif ($mobile['b_id']==3) {
+            $mobile['b_id'] = '配件';
+        }
+
+        $ips=Db::table('goods')
+            ->where(['brand_id'=>$id,'is_show'=>1])
+            ->order('shop_price','esc')
+            ->paginate(8);
+
+        $parts=Db::table('goods')
+            ->alias('a')
+            ->join('brand w','a.cat_id = w.b_id')
+            ->where(['w.b_id'=>4])
+            ->paginate(2);
+
+        return $this->fetch('saler',['mobile'=>$mobile,'ips'=>$ips,'parts'=>$parts]);
     }
 
-    /**
-     * 保存更新的资源
-     *
-     * @param  \think\Request  $request
-     * @param  int  $id
-     * @return \think\Response
+    /*
+     * 按评论排序
      */
-    public function update(Request $request, $id)
+    public function tell($id)
     {
-        //
+        $mobile=Db::table('brand')->where(['id'=>$id,'is_show'=>1])->find();
+        if($mobile['b_id']==1){
+            $mobile['b_id']='电脑';
+        }elseif ($mobile['b_id']==2){
+            $mobile['b_id']='手机';
+        }elseif ($mobile['b_id']==3) {
+            $mobile['b_id'] = '配件';
+        }
+//        var_dump($mobile);die;
+
+        $ips=Db::table('goods')
+            ->alias('a')
+            ->join('brand w','a.brand_id = w.id')
+            ->join('comment c','a.goods_id = c.goods_id')
+            ->where(['a.brand_id'=>$id])
+            ->order('comment_rank')
+            ->paginate(8);
+//        var_dump($ips);die;
+
+        $parts=Db::table('goods')
+            ->alias('a')
+            ->join('brand w','a.cat_id = w.b_id')
+            ->where(['w.b_id'=>4])
+            ->paginate(2);
+
+        return $this->fetch('saler',['mobile'=>$mobile,'ips'=>$ips]);
     }
 
     /**
@@ -100,8 +191,12 @@ class Article extends Controller
      * @param  int  $id
      * @return \think\Response
      */
-    public function delete($id)
+    public function login()
     {
-        //
+        if(Session::has('user_name')){
+            $this->success('订单生成中',url('Order/index'));
+        }else{
+            $this->success('未登录，请登录后购买！',url('Login/login'));
+        }
     }
 }
