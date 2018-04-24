457f94294be971f9855f4309ab119d52f600a43a
wenyi <wenyi@qq.com>
2018-04-24 14:15:32 +0800

    4242

--- application/myshop/controller/Article.php
+++ application/myshop/controller/Article.php
@@ -36,13 +36,11 @@ class Article extends Controller
     {
         $content=Db::table('article')->where(['is_show'=>1])->select();
         foreach ($content as & $v) {
-            $v['tab_name'] = Db::table($v['table_name'])->where('id',1)->select();
-            $v['caputer']=Db::table($v['table_name'])->where('id',2)->select();
+            $v['tab_name'] = Db::table($v['table_name'])->where('tab_id',1)->select();
+            $v['caputer']=Db::table($v['table_name'])->where('tab_id',2)->select();
         }
         $brands=Db::table('brand')->where('b_id',2)->select();
         $brandd=Db::table('brand')->where('b_id',1)->select();
-
-
         $iphone=Db::table('goods')
             ->where(['cat_id'=>2,'is_show'=>1])
             ->limit(8)
@@ -74,24 +72,11 @@ class Article extends Controller
             ->limit(8)
             ->select();
         $rexo=Db::table('goods')->where('click_name','>','10')->limit(3)->select();
-        return $this->fetch('index',['iphone'=>$iphone,'brand'=>$brand,'content' => $content,'bdi'=>$brandsdi,'cp'=>$computer,'bdb'=>$brandss,'ips'=>$iphone_s,'bdbd'=>$brandsss,'cps'=>$computer_s
+        $brandp=Db::table('brand')->where('b_id',3)->select();
+        return $this->fetch('index',['iphone'=>$iphone,'brand'=>$brand,'content' => $content,'bdp'=>$brandp,'bdi'=>$brandsdi,'cp'=>$computer,'bdb'=>$brandss,'ips'=>$iphone_s,'bdbd'=>$brandsss,'cps'=>$computer_s
         ,'bd'=>$brands,'bdd'=>$brandd,'rexo'=>$rexo]);
     }
 
-    /**
-     * 显示商品详情
-     *
-     * @return \think\Response
-     */
-    public function eachdi($id){
-        $data=Db::name('goods')->find($id);
-//        var_dump($data);die;
-        $brand=Db::name('brand')->find($data['brand_id']);
-        $run=Db::name('processor')->find($data['runme_id']);
-        $pro=Db::name('processor')->find($data['pro_id']);
-        $mem=Db::name('running_memory')->find($data['memory_id']);
-        return $this->fetch('eachdi',['data'=>$data,'brand'=>$brand,'pro'=>$pro,'run'=>$run,'mem'=>$mem]);
-    }
 
 
 
@@ -117,7 +102,7 @@ class Article extends Controller
             ->where(['w.b_id'=>4])
             ->paginate(2);
 //        var_dump($parts);die;
-        return $this->fetch('salerdi',['mobile'=>$mobile,'ips'=>$ips,'parts'=>$parts]);
+        return $this->fetch('saler',['mobile'=>$mobile,'ips'=>$ips,'parts'=>$parts]);
     }
 
     /**
@@ -250,5 +235,28 @@ class Article extends Controller
         }else{
             $this->success('未登录，请登录后购买！',url('Login/login'));
         }
+    }
+    public function search()
+    {
+        $content=$_POST['index_none_header_sysc'];
+        $sql="select `brand_name` from `brand` where `brand_name` like '%".$content."%'";
+        $brand=Db::table('brand')->query($sql);
+        $number="select count(*) from `goods` where `goods_name` like '%".$content."%'" ;
+        $num=Db::table('goods')->query($number);
+        $cat="select goods_name from `goods` where `goods_name` like '%".$content."'" ;
+        $cate=Db::table('goods')->query($cat);
+        $redn=Db::table('goods')->where('goods_name',['like','$content%'],['like','%$content'])->where('click_name',['>',10],['<>',20],'or')->select();
+        return $this->fetch('search',['content'=>$content,'brand'=>$brand,'num'=>$num,'cate'=>$cate,'redn'=>$redn]);
+    }
+    public function table($id,$table)
+    {
+        //
+        $tabl=Db::table($table)->where('tab_id',$id)->select();
+//        $goo=Db::table('correlation')->find('id',$tabl['id'])['goods_id'];
+//        $goos=Db::table('goods')->where('goods_id',$goo)->select();
+//        var_dump($goo);
+//        die;
+        return $this->fetch('sch',['table'=>$tabl]);
+
     }
 }
